c89c4d39ff27114b511942c77cebe4a7d959be76
batann <122584289+batann@users.noreply.github.com>
2024-09-06 12:11:04 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 if [[ -f /etc/hosts ]]; then
+touch lol
+cat /etc/hosts >> lol
+cat /home/$USER/hosts/hosts >> lol
 sudo mv /etc/hosts /etc/hosts.$(date +%j).org
-sudo mv hosts /etc/hosts
-cd $HOME
+sudo mv lol /etc/hosts
+sudo chown root:root hosts
+cd /home/$USER/
 sudo trash $HOME/hosts
 else
-sudo mv hosts /etc/hosts
-cd $HOME
-sudo trash $HOME/hosts
+sudo mv /home/$USER/hosts/hosts /etc/hosts
+cd /home/$USER/
+sudo trash /home/$user/hosts
 fi
 clear
 echo -e "\033[31mScript executed successfully\033[0m"
+read -n1 -p "Enter [ANY] to continue..." lol
+exit 0
