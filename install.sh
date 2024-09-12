94d66d9d110886c91f69d5b452cb280a81b4491d
batann <122584289+batann@users.noreply.github.com>
2024-09-12 17:56:19 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-if [[ -f /etc/hosts ]]; then
-touch lol
-cat /etc/hosts >> lol
-cat /home/$USER/hosts/hosts >> lol
+touch hosts
+cat /etc/hosts >> hosts
+cat /home/batan/hosts/hosts >> hosts
 sudo mv /etc/hosts /etc/hosts.$(date +%j).org
-sudo mv lol /etc/hosts
 sudo chown root:root hosts
-cd /home/$USER/
-sudo trash $HOME/hosts
-else
-sudo mv /home/$USER/hosts/hosts /etc/hosts
-cd /home/$USER/
-sudo trash /home/$user/hosts
-fi
+sudo mv hosts /etc/hosts
+sudo trash /home/batan/hosts
 clear
 echo -e "\033[31mScript executed successfully\033[0m"
+echo -e "\033[34m Moved original file to \033[31m/etc/hosts.$(date +%j).org\033[0m"
 read -n1 -p "Enter [ANY] to continue..." lol
 exit 0
